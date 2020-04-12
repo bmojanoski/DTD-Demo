52dc3e48de70b6a3e31305bff0f51ae151721304
bmojanoski <blagojche.mojanoski@gmail.com>
2020-04-13 01:37:16 +0200

    Updates

---
+++ precache-manifest.170ebeaa7836275f1a176d090287d764.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99311f6b52cfbb857adddb3252ab26fa",
+    "url": "/DTD-Demo/index.html"
+  },
+  {
+    "revision": "2d01c2f49dbf6a2c928f",
+    "url": "/DTD-Demo/static/css/2.df61ddf0.chunk.css"
+  },
+  {
+    "revision": "74ea94bd434ed9a421bc",
+    "url": "/DTD-Demo/static/css/main.4963eaad.chunk.css"
+  },
+  {
+    "revision": "2d01c2f49dbf6a2c928f",
+    "url": "/DTD-Demo/static/js/2.c109c456.chunk.js"
+  },
+  {
+    "revision": "1b6819df9023f3e10a5e7254002b1a55",
+    "url": "/DTD-Demo/static/js/2.c109c456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ea94bd434ed9a421bc",
+    "url": "/DTD-Demo/static/js/main.c56dde66.chunk.js"
+  },
+  {
+    "revision": "99c707a1061504e2240b",
+    "url": "/DTD-Demo/static/js/runtime-main.d6fbcb4d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/DTD-Demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/DTD-Demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/DTD-Demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/DTD-Demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/DTD-Demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
